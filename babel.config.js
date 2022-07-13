816ba77bdf3b6cbd655468a7be098973782bb6b1
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2022-07-13 07:38:25 -0500

    Heroku 2

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
-  ,
-  devServer: {
-    port:8080,
-    proxy: {
-      '/naves':{
-        target: 'http://localhost:8081',
-        ws: true, 
-        changeOrigin:true
-      }
-    }
-  }
+  ]  
 }
